0f621b68a7d0b5e769dbeb324622c8260f9f0998
Maks Solovev <msol86@mail.ru>
2024-03-17 13:05:17 +0500

    release

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,29 +1,57 @@
-//import Swiper from "./swiper.js";
+//Инициализируем слайдер
 
-/*const swiper = new Swiper('.brands-slider');*/
-
-const swiper = new Swiper('.brands-slider', {
+const swiper = new Swiper('.brands__slider', {
   
-   // loop: true,
-  
-    // If we need pagination
-    pagination: {
+    pagination: {                 // Включаем пагинацию
       el: '.swiper-pagination',
     },
   
-    //autoHeight: true,
-    spaceBetween: 20,
-    slidesPerView: 1.1,
-    centeredSlides: false,
-    roundLengths: true,
-   // height: 100,
-    //effect: 'cards',
-  //cardsEffect: {
-    // ...
-  //},
+    spaceBetween: 10,             //Задаем гэп между слайдами
+    slidesPerView: 1.1,           //Показываем часть следующего слайда
+    centeredSlides: false,        //Текущий слайд не центрируем
   });
 
- /* if (document.documentElement.clientWidth >= 768) {
-    swiper.destroy();
+//Делаем обработку кнопки "Показать всё"
+
+let buttonShow = document.querySelector('.brands__button-show');
+let brandsMenu = document.querySelector('.brands__menu');
+
+const countCards_768 = 6;      //Число карточек на экране 768
+const countCards_1119 = 8;     //Число карточек на экране 1119
+
+function brandsShowHide() {
+
+  if ( buttonShow.textContent == 'Показать всё' ) {     //Если кнопка в состоянии "Показать все"
+    
+    buttonShow.textContent = 'Скрыть';
+    buttonShow.classList.toggle('brands__button-show--state-button-hide');
+
+    for (let i = 0; i < brandsMenu.children.length; i++) {
+      brandsMenu.children[i].classList.remove('brands__card--state-768');
+      brandsMenu.children[i].classList.remove('brands__card--state-1119');
+    }
+
+    brandsMenu.classList.add('brands__menu--state-big');
+    
+    return;
+  } 
+  
+  if ( buttonShow.textContent == 'Скрыть' ) {       //Если кнопка в состоянии "Скрыть" if ( buttonShow.textContent == 'Скрыть всё' ) 
+    
+    buttonShow.textContent = 'Показать всё';
+    buttonShow.classList.toggle('brands__button-show--state-button-hide');
+
+    brandsMenu.classList.remove('brands__menu--state-big');
+      
+    for (let i = countCards_768; i < countCards_1119; i++) {
+      brandsMenu.children[i].classList.add('brands__card--state-768');
+    }
+
+    for (let i = countCards_1119; i < brandsMenu.children.length; i++) {
+      brandsMenu.children[i].classList.add('brands__card--state-1119');
+    }
+   
   }
-*/
+}
+
+buttonShow.addEventListener('click', brandsShowHide);
